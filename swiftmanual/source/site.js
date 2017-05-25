5e39eac551bff0426ea37d0431ce16ddfd703ac8
happy <931535958@qq.com>
2017-05-25 21:03:20 +0800

    测试

--- swiftmanual/source/site.js
+++ swiftmanual/source/site.js
@@ -4,7 +4,9 @@
 (function (win) {
     var url = win.location.href,
         h1Link = document.querySelector('h1 a');
-    h1Link.href = url.substr(0, url.indexOf('/manual'));
+    var href = url.substr(0, url.indexOf('/swiftmanual'));
+    href  = href + '/swiftmanual/index.html'
+    h1Link.href = href;
 })(window);
 
 
